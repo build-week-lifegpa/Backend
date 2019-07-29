26712f853bf5199f79022b664cb1604a5c5bcc49
tjr226 <tjr226@gmail.com>
2019-07-29 16:19:51 -0400

    auth in progress

--- api/server.js
+++ api/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
 
 const server = express();
 
 server.use(express.json());
+server.use(cors());
+server.use(express.json());
+
+
 
 server.get('/', (req, res) => {
     res.status(200).json({ api: 'API is working'});
