3a47102467cdae3687a3dbc7ab65dc50cf8b8358
tjr226 <tjr226@gmail.com>
2019-07-30 20:37:16 -0400

    added more middleware

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,7 +1,12 @@
+const categoryDB = require('../categories/category-model.js');
+const habitsDB = require('../habits/habits-model.js');
+const usersDB = require('../users/users-model.js');
+
 module.exports = {
     validateCategory: function (req, res, next) {
         const categoryInfo = req.body;
         if (
+            categoryInfo === undefined ||
             categoryInfo.category_name === undefined ||
             categoryInfo.user_id === undefined
         ) {
@@ -9,7 +14,34 @@ module.exports = {
         } else {
             next();
         }
-    }
+    },
 
-    
+    validateHabit: function (req, res, next) {
+        const habitInfo = req.body;
+        if (
+            habitInfo === undefined ||
+            habitInfo.habit_text === undefined ||
+            habitInfo.user_id === undefined ||
+            habitInfo.category_id === undefined
+        ) {
+            return res.status(400).json({ errorMessage: "Please provide habit text, user id, and category id." });
+        } else {
+            next();
+        }
+    },
+
+    validateUser: function (req, res, next) {
+        const userInfo = req.body;
+        if (
+            userInfo === undefined ||
+            userInfo.username === undefined ||
+            userInfo.password === undefined
+        ) {
+            return res.status(400).json({ errorMessage: "Please provide username and password." });
+        } else {
+            next();
+        }
+    },
+
+
 }
